9ef395d1928161ed49f193a27dfd185dded2756e
Umadevi-Rajamanoharan <54718230+Umadevi-Rajamanoharan@users.noreply.github.com>
2019-09-03 18:03:43 +0530

    Add files via upload

---
+++ list_operations.py
@@ -0,0 +1,17 @@
+#creating list-1
+list1=[1,2,3,4]
+#creating list-2
+list2=[5,6,7,8]
+#printing lists
+print("list-1 ",list1)
+print("list-2 ",list2)
+#in and not in operation
+print("2 present in list-1  ",2 in list1)
+print("10 not present in list-2  ",10 not in list2)
+#copying n times
+print("printing list-1 for 2 times")
+print(2*list1)
+#list concatenation using '+'
+print("printing list-1 and list-2")
+print(list1+list2)
+
